3e4404c04053e94a96ffdd42bda4b3e4ec8fdd56
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2022-01-10 16:11:00 -0300

    add layout MovieCard component

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,11 +1,35 @@
-import { MovieScore } from "component/MovieScore";
+import { MovieCard } from "component/MovieCard";
 import { Pagination } from "component/Pagination";
 
 export function Listing() {
   return (
     <>
       <Pagination />
-      <MovieScore />
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+        </div>
+      </div>
     </>
   );
 }
