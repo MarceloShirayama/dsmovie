1d714a8fd235dddd9c4ba02cd0eadaff362b6fdc
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2022-01-14 13:14:21 -0300

    fetch data from the backend with axios using hooks useEffect and useState

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,9 +1,25 @@
+import { useEffect, useState } from "react";
 import { MovieCard } from "component/MovieCard";
 import { Pagination } from "component/Pagination";
+import axios from "axios";
+import { MoviePage } from "types/movie";
+import { BASE_URL } from "utils/requests";
 
 export function Listing() {
+  const [pageNumber, setPageNumber] = useState(0);
+
+  useEffect(() => {
+    axios.get(`${BASE_URL}/movies?size=12&page=1`).then((res) => {
+      const data = res.data as MoviePage;
+      console.log(data);
+
+      setPageNumber(data.number);
+    });
+  }, []);
+
   return (
     <>
+      <p>pageNumber: {pageNumber}</p>
       <Pagination />
       <div className="container">
         <div className="row">
